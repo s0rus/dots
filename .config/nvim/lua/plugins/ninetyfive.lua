fd28742ab81d4755fa8931f1540dcdc351b103e9
Piotr 'soruse' Mól <piotrmolkontakt@gmail.com>
2026-01-13 19:44:53 +0100

    ✨ use supermaven instead of ninetyfive

--- .config/nvim/lua/plugins/ninetyfive.lua
+++ .config/nvim/lua/plugins/ninetyfive.lua
@@ -1,22 +1,23 @@
 return {
-  "ninetyfive-gg/ninetyfive.nvim",
-  version = "*",
-  config = function()
-    require("ninetyfive").setup({
-      enable_on_startup = true,
-      debug = false,
-      server = "wss://api.ninetyfive.gg",
-      mappings = {
-        enabled = true,
-        accept = "<Tab>",
-        accept_word = "<A-h>",
-        accept_line = "<A-j>",
-        reject = "<A-w>",
-      },
-      indexing = {
-        mode = "ask",
-        cache_consent = true,
-      },
-    })
-  end,
+  --- Using supermaven for now.
+  -- "ninetyfive-gg/ninetyfive.nvim",
+  -- version = "*",
+  -- config = function()
+  --   require("ninetyfive").setup({
+  --     enable_on_startup = true,
+  --     debug = false,
+  --     server = "wss://api.ninetyfive.gg",
+  --     mappings = {
+  --       enabled = true,
+  --       accept = "<Tab>",
+  --       accept_word = "<A-h>",
+  --       accept_line = "<A-j>",
+  --       reject = "<A-w>",
+  --     },
+  --     indexing = {
+  --       mode = "ask",
+  --       cache_consent = true,
+  --     },
+  --   })
+  -- end,
 }
